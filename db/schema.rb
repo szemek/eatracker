2717b14c9a38ae15e646b211da5e8bef0687664a
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2014-12-06 19:27:30 +0100

    meal model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141206112931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meals", force: true do |t|
+    t.string   "name",        null: false
+    t.integer  "energy"
+    t.datetime "consumed_at"
+  end
 
 end
