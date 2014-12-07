6c10368856785c124cfe932a7a9b689ccbf147f2
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2014-12-07 11:27:03 +0100

    improvements in styling; meals#show page

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,8 +1,24 @@
 class MealsController < ApplicationController
+  def index
+    redirect_to(root_path)
+  end
+
   def create
     MealCreator.create(meal_params)
 
     redirect_to(:back)
+  end
+
+  def show
+    meal = Meal.find(params[:id])
+
+    render 'show', locals: {meal: meal}
+  end
+
+  def destroy
+    Meal.destroy(params[:id])
+
+    redirect_to(root_path)
   end
 
   private
