a808b31ee7fcc35072b5dbe5fc6dc12f53813be8
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-12-10 22:44:03 +0100

    simple calculator

--- app/assets/javascripts/calculator.js.jsx
+++ app/assets/javascripts/calculator.js.jsx
@@ -1,10 +1,32 @@
 /*** @jsx React.DOM */
 var APP = React.createClass({
+  getInitialState: function() {
+    return {
+      result: null
+    };
+  },
+  calculate: function() {
+    var a = parseFloat($('#a').val());
+    var b = parseFloat($('#b').val());
+    var c = parseFloat($('#c').val());
+    var result = (b*c)/a;
+
+    this.setState({result: result});
+  },
   render: function() {
     return (
-      <h1>Hello world</h1>
+      <div>
+        <div>
+          <input type='number' placeholder='[g]' value='100' onChange={this.calculate} id='a' />
+          <input type='number' placeholder='[kcal]' onChange={this.calculate} id='b' />
+        </div>
+        <div>
+          <input type='number' placeholder='[g]' onChange={this.calculate} id='c' />
+          <input type='number' placeholder='[kcal]' disabled value={this.state.result} />
+        </div>
+      </div>
     )
   }
 })
 
-React.render(<APP />, document.querySelector('.content'))
+React.render(<APP />, document.querySelector('#calculator'))
